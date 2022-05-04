978fec6367cd7e8bdfeb8f37fa031708afb89978
srinivas bhandage <srini@whyable.com>
2022-05-04 19:03:09 +0530

    added footer

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import {AtomFooterLgComponent} from '../atom-footer-lg/atom-footer-lg.component';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage,
+    AtomFooterLgComponent]
 })
 export class HomePageModule {}
